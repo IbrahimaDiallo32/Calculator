6e116c61e5f1d8080730e4c4390fb26d50483e42
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2024-03-27 00:20:26 -0400

    Progress made on Injecting classes. Log, root, and exponent implemented.

--- Calculator_App/Program.cs
+++ Calculator_App/Program.cs
@@ -1,4 +1,9 @@
+using Calculator_App;
 using Calculator_App.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,3 +30,25 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
+
+
+public class Programs
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((_, services) =>
+            {
+                // Register services here
+                services.AddSingleton<CalculatorEngine>(); // Example registration
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<CalculatorEngine>();
+            });
+}
+
